bbb6f951dc2cb40084051dc5495ba5d3542b66fd
Jenny Chu <jchudesign0930@gmail.com>
2019-04-06 05:22:12 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,5 @@
 class Backer
-  attr_reader :backed_projects, :name
+  attr_accessor :name, :backed_projects
   
   def initialize(name)
     @name = name
@@ -13,17 +13,3 @@ end
 
 end
 
-class Backer
-
-  attr_accessor :name, :backed_projects
-
-  def initialize(name)
-    @name = name
-    @backed_projects = []
-  end
-
-  def back_project(project)
-    @backed_projects << project
-    project.backers << self
-  end
-end
